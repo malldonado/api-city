e1856dcfa1de6e62a138eaa87ee4ab463e54a371
malldonado <matheussilvacontat@gmail.com>
2024-07-16 06:02:56 -0300

    update project

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,7 +1,20 @@
 import express from "express";
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const router = Router();
 
 const server = express();
 
-server.get("/", (req, res) => {});
+interface Teste {}
+
+router.get("/", (_, res) => {
+  return res.send("Hello World!");
+});
+
+router.post("/", (req, res) => {
+  console.log(req);
+  return res.status(StatusCodes.OK).json(req.body);
+});
 
 export { server };
